cc83fe2a4e1db2bcfce0ef7e58216aa6f7c3ac62
Gouresh <gouresh.khochare@neosoftmail.com>
2021-05-28 11:33:55 -0700

    API gateway commit

---
+++ api-gateway-temperaturepoc/src/main/java/com/neosoft/microservices/apigatewaytemperaturepoc/ApiGatewayTemperaturepocApplication.java
@@ -0,0 +1,13 @@
+package com.neosoft.microservices.apigatewaytemperaturepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayTemperaturepocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayTemperaturepocApplication.class, args);
+	}
+
+}
---
+++ api-gateway-temperaturepoc/src/test/java/com/neosoft/microservices/apigatewaytemperaturepoc/ApiGatewayTemperaturepocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosoft.microservices.apigatewaytemperaturepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayTemperaturepocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
