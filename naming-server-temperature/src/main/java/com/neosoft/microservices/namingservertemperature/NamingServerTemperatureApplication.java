6c1dcff26f5576dd9474b0a93c18e74665cf753b
Gouresh <gouresh.khochare@neosoftmail.com>
2021-05-28 11:32:58 -0700

    Naming server commit

---
+++ naming-server-temperature/src/main/java/com/neosoft/microservices/namingservertemperature/NamingServerTemperatureApplication.java
@@ -0,0 +1,14 @@
+package com.neosoft.microservices.namingservertemperature;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class NamingServerTemperatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingServerTemperatureApplication.class, args);
+	}
+
+}
---
+++ naming-server-temperature/src/test/java/com/neosoft/microservices/namingservertemperature/NamingServerTemperatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neosoft.microservices.namingservertemperature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerTemperatureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
